de2a25a30b92ab3107bd6fd018c8874b96b351ec
민우석 <132043249+ws-071211@users.noreply.github.com>
2024-05-22 15:41:46 +0900

    Update dataload.py

--- dataload.py
+++ dataload.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.title('Uber pickups in NYC')
 
@@ -9,13 +10,13 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 # 데이터 불러오기
+@st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
-
 
 # 텍스트 요소 생성. 사용자에게 데이터가 로드 되고 있음을 알린다.
 data_load_state = st.text('Loading data...')
@@ -29,3 +30,22 @@ data_load_state.text('Loading data...done!')
 # 부제목 만들기
 st.subheader('Raw data')
 st.write(data)
+
+# 픽업 횟수 히스토그램 추가
+st.subheader('Number of pickups by hour')
+
+# 시간으로 그룹화
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
+
+# 히스토그램 그래프 생성
+fig, ax = plt.subplots()
+ax.bar(range(24), hist_values, width=0.8)
+ax.set_xlabel('Hour of the day')
+ax.set_ylabel('Number of pickups')
+st.pyplot(fig)
+
+# 맵 시각화 추가
+st.subheader('Map of all pickups')
+
+# 맵 생성
+st.map(data[['lat', 'lon']])
